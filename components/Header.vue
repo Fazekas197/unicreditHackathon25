a2103fbc5f42d870f8f00a61d7319b82e96e2bd7
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-05-16 16:38:56 +0300

    chore: create header

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,30 @@
 <template>
-	<header>
-		<p>header</p>
+	<header class="px-10 py-5 flex justify-between">
+		<div>
+			<NuxtLink to="/" class="space-x-5 flex items-end">
+				<img src="/images/logo.svg" class="h-7" alt="logo Unicredit" />
+				<h4 class="text-xl text-secondary font-extrabold">LEARNING</h4>
+			</NuxtLink>
+		</div>
+		<div class="text-black flex gap-x-10">
+			<NuxtLink to="/" class="flex flex-col items-center gap-y-2">
+				<img src="/icons/diamond.svg" class="h-6" alt="diamond icon" />
+				<p class="font-extrabold">ANTI-FRAUDĂ</p>
+			</NuxtLink>
+			<NuxtLink to="/" class="flex flex-col items-center gap-y-2">
+				<img src="/icons/mobile.svg" class="h-6" alt="diamond icon" />
+				<p class="font-extrabold">MOBILE BANKING</p>
+			</NuxtLink>
+			<NuxtLink to="/" class="flex flex-col items-center gap-y-2">
+				<img src="/icons/money.svg" class="h-6" alt="diamond icon" />
+				<p class="font-extrabold">EDUCAȚIE FINANCIARĂ</p>
+			</NuxtLink>
+		</div>
+		<div class="text-black">
+			<NuxtLink to="/" class="flex flex-col items-center">
+				<img src="/icons/contact.svg" class="h-6" alt="diamond icon" />
+				<p class="font-extrabold">CONTACT</p>
+			</NuxtLink>
+		</div>
 	</header>
 </template>
