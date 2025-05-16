2f9cc2bd5ba326e43746138940c9aab9c7ee3984
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-05-16 13:53:27 +0300

    chore: add data to quiz

--- components/PhisingQuiz.vue
+++ components/PhisingQuiz.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<div class="space-y-10">
 		<div v-for="quiz in phisingQuiz">
 			{{ quiz.test }}
 
--- data/phisingQuiz.js
+++ data/phisingQuiz.js
@@ -1,7 +1,20 @@
 export const phisingQuiz = [
 	{
-		test: "mdaskodoasd",
-		answear: true,
-		explanation: "This is a phishing test.",
+		test: "„Stimate client, vă rugăm să accesați link-ul de mai jos pentru a confirma detaliile contului dvs. pentru a evita suspendarea. Link: http://unicredit-verify-account.security-check.com",
+		answear: false,
+		explanation:
+			"Acest link NU aparține UniCredit! Observi domeniul dubios? E un semnal clar de phishing.",
+	},
+	{
+		test: "„UniCredit: Sesiune suspectă detectată! Accesați urgent portalul de verificare pentru a bloca accesul neautorizat.” Link: http://unicredit-secure-alert-login-update.com",
+		answear: false,
+		explanation:
+			"Link-ul lung și complicat e o păcăleală. Domeniul NU e oficial. E un SMS clasic de phishing.",
+	},
+	{
+		test: "„Stimate client, a fost emisă o nouă factură pentru serviciile UniCredit. Descărcați documentul de mai jos pentru detalii.” Link: http://unicredit-billing-documents-download.com",
+		answear: false,
+		explanation:
+			"UniCredit nu trimite facturi prin astfel de link-uri externe. Domeniul e fals, gândit să păcălească vizual.",
 	},
 ];
