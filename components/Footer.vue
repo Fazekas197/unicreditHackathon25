7e547843b8ef2b3974e8ca6613bdc1cc46399bba
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-05-16 20:01:54 +0300

    chore: create footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,29 @@
 <template>
-	<footer>
-		<p>footer</p>
+	<footer class="px-10 pt-10 pb-7 bg-blackBg text-white flex justify-between">
+		<div class="space-y-2.5">
+			<img src="/images/LogoWhite.svg" alt="Unicredit Logo" />
+			<p class="text-sm text-k3">
+				© 20XX – 20XX UniCredit S.p.A. - All Rights reserved
+			</p>
+		</div>
+		<div class="flex flex-col items-end space-y-2">
+			<div class="flex gap-x-5">
+				<img src="/icons/titok.svg" class="w-6" alt="tik tok" />
+				<img src="/icons/x.svg" class="w-6" alt="x" />
+				<img src="/icons/linkedin.svg" class="w-6" alt="linked in" />
+				<img src="/icons/ytb.svg" class="w-6" alt="youtube" />
+				<img src="/icons/instagram.svg" class="w-6" alt="instagram" />
+				<img src="/icons/facebook.svg" class="w-6" alt="facebook" />
+			</div>
+			<div class="text-lg space-x-5 flex">
+				<a href="" class="text-k3 underline hover:text-secondary"
+					>Interactive</a
+				>
+				<p>|</p>
+				<a href="" class="text-k3 underline hover:text-secondary"
+					>Interactive</a
+				>
+			</div>
+		</div>
 	</footer>
 </template>
