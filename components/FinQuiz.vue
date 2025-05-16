b2fc47ad9b26f292d88b3080788d1def0ce8222c
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-05-16 18:27:58 +0300

    chore: add fin quiz

---
+++ components/FinQuiz.vue
@@ -0,0 +1,7 @@
+<template>
+	<div></div>
+</template>
+
+<script setup>
+	import { finQuiz } from "~/data/finQuiz";
+</script>
---
+++ data/finQuiz.js
@@ -0,0 +1,12 @@
+export const finQuiz = [
+	{
+		question: "Ce este creditul ipotecar?",
+		options: [
+			"Un tip de credit destinat achiziționării unei locuințe, garantat cu imobilul achiziționat.",
+			"Un tip de credit pentru achiziționarea de bunuri de consum.",
+			"Un tip de credit pentru studii universitare.",
+			"Un tip de credit pentru călătorii în străinătate.",
+		],
+		answer: "Un tip de credit destinat achiziționării unei locuințe, garantat cu imobilul achiziționat.",
+	},
+];
