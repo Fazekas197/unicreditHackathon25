6f1dffde645c8e3af5e59e3d4864145aa00f47af
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-05-16 12:57:18 +0300

    chore: add data

---
+++ data/phisingQuiz.js
@@ -0,0 +1,6 @@
+export const phisingQuiz = [
+	{
+		test: "mdaskodoasd",
+		answer: true,
+	},
+];
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
 	<div>
 		<h1 class="text-9xl">wow</h1>
+		{{ questions }}
 	</div>
 </template>
+
+<script setup>
+	import { phisingQuiz } from "../data/phisingQuiz.js";
+	const questions = phisingQuiz;
+</script>
