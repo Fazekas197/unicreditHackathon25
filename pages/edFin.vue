fc3683be998a503fc83b6e66e8af3f2c0be2e949
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-05-17 14:44:59 +0300

    chore: finish ed fin page

--- pages/edFin.vue
+++ pages/edFin.vue
@@ -16,7 +16,7 @@
 				class="bg-blackBg text-white space-y-15 px-24 py-24"
 			>
 				<h1 class="text-4xl">
-					Este sau nu phishing? - Testează-ți cunoștințele
+					Cât de multe știi despre credite? - Află acum
 				</h1>
 				<FinQuiz />
 			</section>
@@ -73,6 +73,79 @@
 				<p>
 					dacă vrei să înlocuiești un credit vechi cu unul mai
 					avantajos
+				</p>
+			</div>
+			<USeparator class="my-5" />
+		</section>
+
+		<section class="flex gap-x-10 px-24 py-24">
+			<div>
+				<img
+					src="/images/payment.png"
+					alt="Atac Hacker"
+					class="w-full"
+				/>
+			</div>
+			<div class="space-y-3 w-2/5">
+				<h1 class="text-3xl">Ce este dobânda?</h1>
+				<h3 class="text-lg font-extrabold">
+					Dobânda este „prețul” pe care îl plătești pentru că
+					folosești banii băncii.
+				</h3>
+				<p class="text-lg">
+					Pentru a pune lucrurile într-o perspectivă mai concretă,
+					oferim și un exemplu: Dacă iei
+					<b class="font-extrabold">10.000 lei de la bancă</b> și
+					trebuie să
+					<b class="font-extrabold">returnezi 11.200 lei în total</b>,
+					înseamnă că<b class="font-extrabold">
+						1.200 lei e costul creditului</b
+					>
+				</p>
+				<div>
+					<p class="text-lg">
+						Dobândă fixă = rămâne la fel toată perioada.
+					</p>
+					<p class="text-lg">
+						Dobândă variabilă = se poate schimba (poate crește sau
+						scădea)
+					</p>
+				</div>
+			</div>
+		</section>
+
+		<section class="px-24 py-24">
+			<h1 class="pb-10 text-3xl text-center w-full">
+				Pașii pentru un credit ipotecar la UniCredit
+			</h1>
+			<USeparator class="my-5" />
+			<div class="text-lg space-y-3">
+				<p class="font-extrabold">
+					1. Mergi la bancă și discută cu un consultant
+				</p>
+			</div>
+			<USeparator class="my-5" />
+			<div class="text-lg space-y-3">
+				<p class="font-extrabold">
+					2. Depui cererea și actele (venituri, buletin, imobil)
+				</p>
+			</div>
+			<USeparator class="my-5" />
+			<div class="text-lg space-y-3">
+				<p class="font-extrabold"> 3. Evaluarea locuinței</p>
+			</div>
+			<USeparator class="my-5" />
+			<div class="text-lg space-y-3">
+				<p class="font-extrabold"> 4. Primești răspuns de aprobare</p>
+			</div>
+			<USeparator class="my-5" />
+			<div class="text-lg space-y-3">
+				<p class="font-extrabold"> 5. Semnezi contractul la notar</p>
+			</div>
+			<USeparator class="my-5" />
+			<div class="text-lg space-y-3">
+				<p class="font-extrabold">
+					 6. Primești banii și începi să plătești rata
 				</p>
 			</div>
 			<USeparator class="my-5" />
