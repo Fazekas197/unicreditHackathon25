0a1b84c6f6f625cb9707770b6aab34b8b062b3c7
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-05-17 12:03:01 +0300

    chore: make hero a component

---
+++ components/Hero.vue
@@ -0,0 +1,31 @@
+<template>
+	<!-- HERO -->
+	<section
+		class="relative before:content-[''] before:absolute before:top-0 before:right-0 before:bottom-0 before:left-0"
+	>
+		<img :src="bgImg" :alt="altImg" class="w-full" />
+		<div class="text-white absolute top-0 mx-24 my-24 space-y-9 w-2/5">
+			<h1 class="text-4xl font-extrabold">
+				{{ title }}
+			</h1>
+			<p class="text-lg">
+				{{ paragraph }}
+			</p>
+			<NuxtLink :to="btnLink">
+				<Button :text="btnText" class="cursor-pointer" />
+			</NuxtLink>
+		</div>
+		<div class="h-2.5 bg-brand"></div>
+	</section>
+</template>
+
+<script setup>
+	const props = defineProps([
+		"title",
+		"paragraph",
+		"btnText",
+		"btnLink",
+		"bgImg",
+		"altImg",
+	]);
+</script>
