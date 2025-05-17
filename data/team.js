16389c3a3fa211427aa5b7378090ff68883d2013
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-05-17 11:50:33 +0300

    chore: init team data

---
+++ data/team.js
@@ -0,0 +1,8 @@
+export const team = [
+	{
+		link: "https://www.linkedin.com/in/cosmin-fazekaș-07a744213/",
+		nume: "Fazekaș Cosmin",
+		email: "fazekascosmin1@gmail.com",
+		poza: "/team/Fazekas.jpg",
+	},
+];
