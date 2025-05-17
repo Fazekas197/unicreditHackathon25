b39b5365e8f87635e239c93a309767252c0ac638
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-05-17 12:07:07 +0300

    chore: add hero to phishing page

--- pages/phishing.vue
+++ pages/phishing.vue
@@ -1,6 +1,17 @@
 <template>
 	<div>
-		<PhishingQuiz />
+		<Hero
+			bg-img="/images/fraudaBg.png"
+			alt-img="Siguranta Online Background"
+			title="Siguranță Online – Fii cu un pas înaintea pericolelor digitale"
+			paragraph="Învață să-ți protejezi informațiile personale și banii împotriva celor mai noi amenințări din mediul digital. Phishing-ul nu e o poveste, e o realitate."
+			btn-text="Start"
+			btn-link="#start"
+		/>
+
+		<section id="start">
+			<PhishingQuiz />
+		</section>
 	</div>
 </template>
 
