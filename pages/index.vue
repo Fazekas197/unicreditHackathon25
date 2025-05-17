f6fbacfcc229ffc2457a07ecf92f6be4c6bfe996
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-05-17 09:53:33 +0300

    chore: create hero section

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,28 @@
 <template>
-	<div></div>
+	<div>
+		<!-- HERO -->
+		<section
+			class="relative before:content-[''] before:absolute before:top-0 before:right-0 before:bottom-0 before:left-0"
+		>
+			<img
+				src="/bg/HomeBg.png"
+				alt="Home Page Background"
+				class="w-full"
+			/>
+			<div class="text-white absolute top-0 mx-24 my-24 space-y-4 w-2/5">
+				<h1 class="text-4xl font-extrabold">
+					UniCredit Learning - Simplu. Clar. Pentru toți.
+				</h1>
+				<p class="text-lg">
+					La UniCredit, credem că încrederea în deciziile financiare
+					începe cu înțelegerea lor. De aceea, am creat un spațiu
+					digital dedicat tuturor celor care vor să își gestioneze
+					banii mai bine — indiferent de vârstă sau nivel de
+					cunoștințe.
+				</p>
+				<Button text="Explorează acum" />
+			</div>
+			<div class="h-2.5 bg-brand"></div>
+		</section>
+	</div>
 </template>
